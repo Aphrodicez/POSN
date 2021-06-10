8a58f0e617c51d267b83f7c8fb0afeb6e5790f55
Aphrodicez <57552210+Aphrodicez@users.noreply.github.com>
2021-06-10 12:44:50 +0700

    Camp3

--- POSN17/Camp3/Maze-TOI8-Kth-State.cpp
+++ POSN17/Camp3/Maze-TOI8-Kth-State.cpp
@@ -2,7 +2,7 @@
     Author	: ~Aphrodicez
     School	: RYW
     Lang	: CPP
-    Algo	: Breadth First Search
+    Algo	: Breadth First Search Kth State
     Status	: Accepted
 */
 
@@ -31,81 +31,85 @@ const int d8i[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 const int d8j[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
 struct GRAPH {
-    int i, j;
+    int i, j, usedbomb;
 };
 
 const int N = 2e2 + 10;
 const int M = 2e5 + 10;
 
-int a[N][N], disst[N][N], disen[N][N];
+int a[N][N];
+
+int disst[2][N][N], disen[2][N][N];
 
 queue <GRAPH> bfs;
 
 void solve() {
+    memset(disst, -1, sizeof disst);
+    memset(disen, -1, sizeof disen);
     int r, c, sti, stj, eni, enj;
     cin >> r >> c;
     cin >> sti >> stj;
     cin >> eni >> enj;
-    for(int i = 1; i <= r; i++) {
-        for(int j = 1; j <= c; j++) {
+    for(int i = 1; i <= r; i++){
+        for(int j = 1; j <= c; j++){
             cin >> a[i][j];
         }
     }
-    memset(disst, -1, sizeof disst);
-    disst[sti][stj] = 0;
-    bfs.push({sti, stj});
+    disst[0][sti][stj] = 0;
+    bfs.push({sti, stj, 0});
     while(!bfs.empty()) {
         int i = bfs.front().i;
         int j = bfs.front().j;
+        int usedbomb = bfs.front().usedbomb;
         bfs.pop();
-        for(int k = 0; k < 4; k++) {
-            int ii = i + d4i[k];
-            int jj = j + d4j[k];
+        for(int k = 0; k < 4; k++){
+            int ii = i + d4i[k], jj = j + d4j[k];
             if(ii < 1 || jj < 1 || ii > r || jj > c)
                 continue;
-            if(disst[ii][jj] != -1)
-                continue;
-            if(!a[ii][jj]){
-                disst[ii][jj] = disst[i][j] + 1;
-                continue;
+            if(a[ii][jj] == 0 && !usedbomb && disst[usedbomb + 1][ii][jj] == -1){
+                disst[usedbomb+1][ii][jj] = disst[usedbomb][i][j] + 1;
+                bfs.push({ii, jj, usedbomb + 1});
             }
-            disst[ii][jj] = disst[i][j] + 1;
-            bfs.push({ii, jj});
+            if(a[ii][jj] == 0)
+                continue;
+            if(disst[usedbomb][ii][jj] != -1)
+                continue;
+            disst[usedbomb][ii][jj] = disst[usedbomb][i][j] + 1;
+            bfs.push({ii, jj, usedbomb});
         }
     }
-    memset(disen, -1, sizeof disen);
-    disen[eni][enj] = 0;
-    bfs.push({eni, enj});
+    disen[0][eni][enj] = 0;
+    bfs.push({eni, enj, 0});
     while(!bfs.empty()) {
         int i = bfs.front().i;
         int j = bfs.front().j;
+        int usedbomb = bfs.front().usedbomb;
         bfs.pop();
-        for(int k = 0; k < 4; k++) {
-            int ii = i + d4i[k];
-            int jj = j + d4j[k];
+        for(int k = 0; k < 4; k++){
+            int ii = i + d4i[k], jj = j + d4j[k];
             if(ii < 1 || jj < 1 || ii > r || jj > c)
                 continue;
-            if(disen[ii][jj] != -1)
-                continue;
-            if(!a[ii][jj]){
-                disen[ii][jj] = disen[i][j] + 1;
-                continue;
+            if(a[ii][jj] == 0 && !usedbomb && disen[usedbomb + 1][ii][jj] == -1){
+                disen[usedbomb+1][ii][jj] = disen[usedbomb][i][j] + 1;
+                bfs.push({ii, jj, usedbomb + 1});
             }
-            disen[ii][jj] = disen[i][j] + 1;
-            bfs.push({ii, jj});
+            if(a[ii][jj] == 0)
+                continue;
+            if(disen[usedbomb][ii][jj] != -1)
+                continue;
+            disen[usedbomb][ii][jj] = disen[usedbomb][i][j] + 1;
+            bfs.push({ii, jj, usedbomb});
         }
     }
-    int ans = 2e9, cnt = 0;
-    for(int i = 1; i <= r; i++) {
-        for(int j = 1; j <= c; j++) {
-            if(!a[i][j] && disst[i][j] != -1 && disen[i][j] != -1) {
+    int cnt = 0;
+    for(int i = 1; i <= r; i++){
+        for(int j = 1; j <= c; j++){
+            if(a[i][j] == 0 && disst[1][i][j] != -1 && disen[1][i][j] != -1)
                 cnt++;
-                ans = min(ans, disst[i][j] + disen[i][j] + 1);
-            }
         }
     }
     cout << cnt << '\n';
-    cout << ans << '\n';
+    cout << disst[1][eni][enj] + 1 << '\n';
 }
 
 int main() {
@@ -126,7 +130,7 @@ void setIO() {
 1 0 1 1 0 1 1 1 
 1 0 1 1 1 0 0 1 
 1 1 0 0 1 0 0 1 
-0 0 1 1 0 1 1 1
+0 0 1 1 0 1 1 1 
 
 6 8 
 1 4 
