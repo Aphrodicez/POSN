d621f721f479af2367468cd249c9589fbce19feb
Chayanin Kongsareekul <kang.chayanin@gmail.com>
2022-10-13 09:19:05 +0700

    Update 62_Cross_Circle.cpp

--- Camp1/Control_Flow_Statement/62_Cross_Circle.cpp
+++ Camp1/Control_Flow_Statement/62_Cross_Circle.cpp
@@ -5,29 +5,41 @@ int main() {
     int q;
     scanf("%d", &q);
     while(q--) {
-        double hs, ks, rs, hb, kb, rb;
-        scanf("%lf %lf %lf %lf %lf %lf", &hs, &ks, &rs, &hb, &kb, &rb);
-        double dist = sqrt((hs - hb) * (hs - hb) + (ks - kb) * (ks - kb));
-        if(rs > rb) {
-            double tmp = rs;
-            rs = rb;
-            rb = tmp;
+        double hs, ks, rmin, hb, kb, rmax;
+        scanf("%lf %lf %lf %lf %lf %lf", &hs, &ks, &rmin, &hb, &kb, &rmax);
+        double dist = sqrt((hs - hb) * (hs - hb) + (ks - kb) * (ks - kb)); // Distance between the Centres
+
+        if(rmin > rmax) {
+            // Swap rmin and rmax if rmin > rmax
+            double tmp = rmin;
+            rmin = rmax;
+            rmax = tmp;
         }
-        if(rb <= dist) {
-            if(rs + rb == dist)
-                printf("One\n");
-            else if(rs + rb > dist)
-                printf("More\n");
-            else if(rs + rb < dist)
-                printf("None\n");
+
+        if(dist == 0) {
+            if(rmax == rmin)
+                printf("More\n"); // (1) Same circle
+            else
+                printf("None\n"); // (2) Ring
         }
-        else {
-            if(rb == rs + dist)
-                printf("One\n");
-            else if(dist + rs > rb)
-                printf("More\n");
-            else if(dist + rs < rb)
-                printf("None\n");
+        else if(rmax == dist) {
+            printf("More\n"); // (3) Small circle's centre on big circle's circumference
+        }
+        else if(rmax < dist) {
+            if(rmin + rmax == dist)
+                printf("One\n"); // (4) 
+            else if(rmin + rmax > dist)
+                printf("More\n"); // (5)
+            else if(rmin + rmax < dist)
+                printf("None\n"); // (6)
+        }
+        else if(rmax > dist) {
+            if(rmax == rmin + dist)
+                printf("One\n"); // (7)
+            else if(dist + rmin > rmax)
+                printf("More\n"); // (8)
+            else if(dist + rmin < rmax)
+                printf("None\n"); // (9)
         }
     }
     return 0;
